50b6fe328859932170cb95043b0769672b64aaa2
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2025-03-24 12:35:22 +0100

    refactor: split init lua into smaller pieces

---
+++ lua/custom/plugins/typescripttools.lua
@@ -0,0 +1,33 @@
+local typescripttools = {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
+  config = function()
+    require('typescript-tools').setup {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+      filetypes = {
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'vue',
+      },
+      settings = {
+        tsserver_plugins = {
+          '@vue/typescript-plugin',
+        },
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { 'javascriptreact', 'typescriptreact' },
+        },
+      },
+    }
+  end,
+}
+return typescripttools
+
+-- npm i -g @vue/typescript-plugin
